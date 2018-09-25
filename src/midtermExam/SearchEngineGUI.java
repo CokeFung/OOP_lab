acdcec8a278416bbe4aea767329aff8327cbfd12
eieieueueoeo <naruebetcm@gmail.com>
2018-09-25 22:19:58 +0700

    fixed SearchEngineGUI

--- src/midtermExam/SearchEngineGUI.java
+++ src/midtermExam/SearchEngineGUI.java
@@ -11,7 +11,9 @@ public class SearchEngineGUI {
 	private JButton bn;
 	private JTextArea ta;
 	private JScrollPane scroll;
-	private JPanel p;
+	private JPanel p1, p2, p3;
+	private JRadioButton r1, r2;
+	private ButtonGroup gb;
 	
 	public static void main(String[] arg) {
 		SearchEngineGUI se = new SearchEngineGUI();
@@ -24,16 +26,31 @@ public class SearchEngineGUI {
 		tf = new JTextField(12);
 		bn = new JButton("GO!");
 		ta = new JTextArea(10, 10);
-		p = new JPanel();
+		p1 = new JPanel();
+		p2 = new JPanel();
+		p3 = new JPanel();
+		r1 = new JRadioButton("Yahoo");
+		r2 = new JRadioButton("Google");
+		gb = new ButtonGroup();
 		scroll = new JScrollPane(ta);
 		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		
-		p.add(la);
-		p.add(tf);
-		p.add(bn);
+		p1.add(la);
+		p1.add(tf);
+		p1.add(bn);
 		
-		frame.add(p);
+		p2.add(r1);
+		p2.add(r2);
+		
+		p3.setLayout(new BorderLayout());
+		p3.add(p1);
+		p3.add(p2, BorderLayout.SOUTH);
+		
+		gb.add(r1);
+		gb.add(r2);
+		
+		frame.add(p3);
 		frame.add(scroll, BorderLayout.SOUTH);
 		
 		frame.setTitle("Search Engine");
